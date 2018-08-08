80bd8592127abd4c22bc1ec1305448173a8bf54b
doanhung95wkm <doanhungb1@wakumo.vn>
2018-08-08 11:58:06 +0700

    fix the test in /spec

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/rbcrypto'
 
 describe Rbcrypto do
-
+  describe "in ruby" do
     it "should show True" do
       Rbcrypto.validate('mpXwg4jMtRhuSpVq4xS3HFHmCmWp9NyGKt','--btc',:testnet).should eql true
     end
@@ -13,4 +13,15 @@ describe Rbcrypto do
     it "should show False" do
       Rbcrypto.validate('0x4bc787699093f11316e819b5692be04a712c4e69','--eth').should eql true
     end
+  end
+
+  describe "on terminal" do
+    it "should show true" do
+      system('rbcrypto validate mpXwg4jMtRhuSpVq4xS3HFHmCmWp9NyGKt --btc').should eql true
+    end
+
+    it "should show false" do
+      system('rbcrypto validate 0xXwg4jMtRhuSpVq4xS3HFHmCmWp9NyGKt --btc').should eql false
+    end
+  end
 end
